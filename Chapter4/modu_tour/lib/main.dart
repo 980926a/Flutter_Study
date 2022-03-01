aa14ef54e055ab9610816a4d90b52e2d2b401325
bohyeon <980926a@gmail.com>
2022-03-01 12:12:30 +0900

    앱 만들기 : 인트로, 회원가입

---
+++ Chapter4/modu_tour/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:modu_tour/signPage.dart';
+import 'login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '모두의 여행',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/sign' : (context) => SignPage(),
+      },
+    );
+  }
+}
